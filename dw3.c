737840c4989a5ccfc3bbde4cc5ae941bce5075b2
John L. Hammond <john.hammond@intel.com>
2013-05-29 14:08:08 -0500

    ... eval.

--- dw3.c
+++ dw3.c
@@ -15,13 +15,15 @@
 #include "frame_info.h"
 #include "list.h"
 
-#define error(info, fmt, args...) fprintf(stderr, fmt, ##args)
+#ifndef ARRAY_LENGTH
+#define ARRAY_LENGTH(a) (sizeof(a) / sizeof((a)[0]))
+#endif
 
 #define xbt_trace(fmt, args...) \
-	error(INFO, "@ %s:%d: "fmt, __func__, __LINE__, ##args)
+	frpintf(stderr, "@ %s:%d: "fmt"\n", __func__, __LINE__, ##args)
 
 #define xbt_error(fmt, args...) \
-	fprintf(stderr, "@ %s:%d: "fmt, __func__, __LINE__, ##args)
+	fprintf(stderr, "@ %s:%d: "fmt"\n", __func__, __LINE__, ##args)
 
 #define xbt_assert(x) (assert(x))
 
@@ -40,47 +42,108 @@ struct xbt_dwfl_module_arg {
 	unsigned long a_text_offset; /* Offset of saved RIP in .text. */
 };
 
+enum {
+	XBT_EVAL_OK,
+	XBT_EVAL_UNDERFLOW,
+	XBT_EVAL_OVERFLOW,
+	XBT_EVAL_BAD_OP,
+	XBT_EVAL_DIV_ERR,
+	XBT_EVAL_MEM_ERR,
+	XBT_EVAL_UNSUPP,
+	XBT_EVAL_TODO,
+};
+
+static bool xbt_eval_abort_on_error = true;
+
+#define DWARF_SWORD_MIN LONG_MIN /* XXX? */
+
+#define XBT_NR_REGS 16
+
 static int xbt_eval(struct xbt_dwfl_module_arg *arg,
-		    Dwarf_Word *val, size_t *val_size,
+		    Dwarf_Word *result, size_t *result_size,
+		    bool *result_is_value, /* Signedness? */
 		    const Dwarf_Op *expr, size_t expr_len)
 {
-	Dwarf_Word v[256];
+	Dwarf_Word s[256];
 	size_t k = 0;
 	int rc;
 
-	*val_size = sizeof(val);
+	*result = 0x4242424242424242UL;
+	*result_size = sizeof(*result);
+	*result_is_value = false;
 
-#define PUSH(v) ((void) 0)
-#define POP() 0
-#define PEEK(i) 0 /* 0 <= i && i < k */
-#define DEREF(p) 0
-
-#define REG(i)
-
-#define ERR(e)					\
-	do  {					\
-		assert((e) > 0);		\
-		rc = -(e);			\
-		goto out;			\
+#define OUT(err)						\
+	do  {							\
+		int _err = (err);				\
+		assert(0 <= _err);				\
+		if (xbt_eval_abort_on_error && _err != 0)	\
+			abort();				\
+		rc = -_err;					\
+		goto out;					\
 	} while (0)
 
-	size_t i;
+#define REQUIRE(cond, err)			\
+	do {					\
+		if (!(cond))			\
+			OUT(err);		\
+	} while (0)
+
+#define PUSH(v)								\
+	do {								\
+		REQUIRE(k < ARRAY_LENGTH(s) - 1, XBT_EVAL_OVERFLOW);	\
+		s[k++] = (v);						\
+	} while (0)
+
+#define POP()						\
+	({						\
+		REQUIRE(0 < k, XBT_EVAL_UNDERFLOW);	\
+		s[--k];					\
+	})
+
+#define PEEK(d)							\
+	({							\
+		size_t _d = (d);				\
+								\
+		REQUIRE(_d < k, XBT_EVAL_UNDERFLOW);		\
+		s[k - _d - 1];					\
+	})
+
+#define MEM(m, size)				\
+	({					\
+		OUT(XBT_EVAL_TODO);		\
+		0;				\
+	})
+
+#define REG(r)					\
+	({					\
+		OUT(XBT_EVAL_TODO);		\
+		0;				\
+	})
+
+/* XXX */
+#define REG_LOC(r)						\
+	do {							\
+		Dwarf_Word _r = (r);				\
+								\
+		REQUIRE(_r < XBT_NR_REGS, XBT_EVAL_BAD_OP);	\
+		PUSH(_r);					\
+	} while (0)
+
+	size_t i = 0;
+	/* Due to skip, i is also modified in the loop body. */
 	for (i = 0; i < expr_len; i++) {
 		uint8_t u = expr[i].unit;
 		Dwarf_Word n0 = expr[i].number;
 		Dwarf_Word n1 = expr[i].number2;
-
+		
 		switch (u) {
+		/* Literal encodings. */
+		case DW_OP_lit0 ... DW_OP_lit31:
+			PUSH(u - DW_OP_lit0);
+			break;
 		case DW_OP_addr:
 			PUSH(n0);
 			break;
-		case DW_OP_deref: {
-			Dwarf_Word p;
-
-			p = POP();
-			PUSH(DEREF(p));
-			break;
-		}
 		case DW_OP_const1u:
 		case DW_OP_const1s:
 		case DW_OP_const2u:
@@ -93,6 +156,23 @@ static int xbt_eval(struct xbt_dwfl_module_arg *arg,
 		case DW_OP_consts:
 			PUSH(n0);
 			break;
+
+		/* Register based addressing. */
+		case DW_OP_fbreg: {
+			Dwarf_Word b;
+
+			b = FRAME_BASE();
+			PUSH(b + n0);
+			break;
+		}
+		case DW_OP_breg0 ... DW_OP_breg31:
+			PUSH(REG(u - DW_OP_breg0) + n0);
+			break;
+		case DW_OP_bregx:
+			PUSH(REG(n0) + n1);
+			break;
+
+		/* Stack operations. */
 		case DW_OP_dup:
 			PUSH(PEEK(0));
 			break;
@@ -125,21 +205,46 @@ static int xbt_eval(struct xbt_dwfl_module_arg *arg,
 			PUSH(t1);
 			break;
 		}
-		case DW_OP_xderef: {
-			/* The second stack entry is treated as an
-			   "address space identifier" for those
-			   architectures that support multiple address
-			   spaces. */
-			ERR(ENOSYS);
+
+		/* Dereference operations. */
+		case DW_OP_deref: {
+			Dwarf_Word t0;
+
+			t0 = POP();
+			PUSH(MEM(t0, sizeof(t0)));
 			break;
 		}
-		case DW_OP_abs: {
-			Dwarf_Word t;
+		case DW_OP_deref_size: {
+			Dwarf_Word t0;
 
-			t = POP();
-			if ((DWarf_Sword) t < 0)
-				t = -t;
-			PUSH(t);
+			t0 = POP();
+			PUSH(MEM(t0, n0));
+			break;
+		}
+		case DW_OP_xderef:
+			/* Extended dereference: t0 is address, t1 is
+			 * address space identifier. */
+			OUT(XBT_ERR_UNSUPP); /* Not emitted AFAIK. */
+		case DW_OP_xderef_size:
+			/* Same as xderef excpet that n0 is the value size. */
+			OUT(XBT_ERR_UNSUPP); /* Not emitted AFAIK. */
+		case DW_OP_push_object_address:
+			/* For 'this'? */
+			OUT(XBT_ERR_UNSUPP); /* Not emitted AFAIK. */
+		case DW_OP_form_tls_address:
+			OUT(XBT_ERR_TODO); /* Not emitted AFAIK. */
+		case DW_OP_call_frame_cfa:
+			/* CFA computed from CFI. */
+			OUT(XBT_ERR_TODO); /* Not emitted AFAIK. */
+
+		/* Arithmetic and logical operations. */
+		case DW_OP_abs: {
+			Dwarf_Word t0;
+
+			t0 = POP();
+			if ((Dwarf_Sword) t0 < 0)
+				t0 = -t0;
+			PUSH(t0);
 			break;
 		}
 		case DW_OP_and: {
@@ -152,14 +257,12 @@ static int xbt_eval(struct xbt_dwfl_module_arg *arg,
 		}
 		case DW_OP_div: {
 			Dwarf_Sword t0, t1;
-#define DWARF_SWORD_MIN LONG_MIN /* XXX? */
 
 			t0 = POP();
 			t1 = POP();
-			/* Is this right? */
-			if (t0 == 0 || (t1 == DWARF_SWORD_MIN && t0 == -1))
-				ERR(ERANGE);
-				
+			/* TODO CHECKME */
+			REQUIRE(t0 != 0 && (t0 != -1 || t1 != DWARF_SWORD_MIN),
+				XBT_EVAL_DIV_ERR);
 			PUSH(t1 / t0);
 			break;
 		}
@@ -176,10 +279,9 @@ static int xbt_eval(struct xbt_dwfl_module_arg *arg,
 
 			t0 = POP();
 			t1 = POP();
-			/* Right? */
-			if (t0 == 0 || (t1 == DWARF_SWORD_MIN && t0 == -1))
-				ERR(ERANGE);
-
+			/* TODO CHECKME */
+			REQUIRE(t0 != 0 && (t0 != -1 || t1 != DWARF_SWORD_MIN),
+				XBT_EVAL_DIV_ERR);
 			PUSH(t1 % t0);
 			break;
 		}
@@ -261,9 +363,8 @@ static int xbt_eval(struct xbt_dwfl_module_arg *arg,
 			PUSH(t1 ^ t0);
 			break;
 		}
-		case DW_OP_bra:
-			ERR(ENOSYS); /* FIXME. */
-			break;
+
+		/* Control flow operations. */
 		case DW_OP_eq: {
 			Dwarf_Sword t0, t1;
 
@@ -312,67 +413,87 @@ static int xbt_eval(struct xbt_dwfl_module_arg *arg,
 			PUSH(t1 != t0);
 			break;
 		}
-		case DW_OP_skip:
-			ERR(ENOSYS); /* FIXME. */
-			break;
-		case DW_OP_lit0 ... DW_OP_lit31:
-			PUSH(u - DW_OP_lit0);
-			break;
-		case DW_OP_reg0 ... DW_OP_reg31:
-			PUSH(REG(u - DW_OP_reg0));
-			break;
-		case DW_OP_breg0 ... DW_OP_breg31:
-			/* TODO */
-			break;
-		case DW_OP_regx:
-			/* TODO */
-			break;
-		case DW_OP_fbreg: {
-			Dwarf_Word t;
+		case DW_OP_skip: do_skip: {
+			Dwarf_Word off;
+			size_t j;
 
-			t = FRAME_BASE();
-			PUSH(t + n0);
+			/* CHECKME */
+			off = expr[i].offset + 3 + (int16_t)n0;
+			for (j = 0; j < expr_len; j++)
+				if (expr[j].offset == off)
+					break;
+
+			if (j == expr_len)
+				OUT(XBT_EVAL_BAD_OP);
+
+			i = j - 1;
 			break;
 		}
-		case DW_OP_bregx:
-			PUSH(REG(n0) + n1); /* ? */
-			break;
-		case DW_OP_piece:
-		case DW_OP_deref_size:
-			/* ... */
-		case DW_OP_xderef_size:
-			/* ... */
-		case DW_OP_nop:
-			break;
+		case DW_OP_bra: {
+			Dwarf_Word t0;
 
-		case DW_OP_push_object_address:
+			t0 = POP();
+			if (t0 != 0)
+				goto do_skip;
+			break;
+		}
 		case DW_OP_call2:
 		case DW_OP_call4:
 		case DW_OP_call_ref:
-		case DW_OP_form_tls_address:
-			ERR(ENOSYS);
+			OUT(XBT_ERR_UNSUPP); /* Not emitted AFAIK. */
 
-		case DW_OP_call_frame_cfa:
-		case DW_OP_bit_piece:
+		/* Special operations. */
+		case DW_OP_nop:
+			break;
+
+		/* Register locations. */	
+		case DW_OP_reg0 ... DW_OP_reg31:
+			/* These operations name a register location. */
+			PUSH(REG_LOC(u - DW_OP_reg0));
+			break;
+		case DW_OP_regx
+			/* This operation names a register location. */
+			PUSH(REG_LOC(n0));
+			break;
+
+		/* Implicit locations. */
 		case DW_OP_implicit_value:
+		/* The DW_OP_implicit_value operation specifies an
+		   immediate value using two operands: an unsigned
+		   LEB128 length, followed by a block representing the
+		   value in the memory representation of the target
+		   machine. The length operand gives the length in
+		   bytes of the block. */
+			OUT(XBT_ERR_TODO) /* Not emitted AFAIK. */
 
 		case DW_OP_stack_value: {
+			/* FIXME this is wrong for composite locations. */
 			*val = POP();
 			rc = 0;
 			goto out;
 		}
+
+		/* Empty location descriptions.
+		   An empty location description consists of a DWARF
+		   expression containing no operations. It represents
+		   a piece or all of an object that is present in the
+		   source but not in the object code (perhaps due to
+		   optimization). */
+
+		/* Composite location descriptions. */
+		case DW_OP_piece:
+		case DW_OP_bit_piece:
+			OUT(XBT_EVAL_TODO); /* Rarely emimtted. */
+
+		/* GNU extensions. */
 		case DW_OP_GNU_push_tls_address:
 		case DW_OP_GNU_uninit:
 		case DW_OP_GNU_encoded_addr:
 		case DW_OP_GNU_implicit_pointer:
+			OUT(XBT_EVAL_UNSUPP); /* Not emitted AFAIK. */
+
 		default:
-			ERR(ENOSYS); /* FIXME. */
-			break;
-
-			// DW_OP_lo_user:
-			// DW_OP_hi_user:
-
-
+			OUT(XBT_EVAL_BAD_OP);
 		}
 	}
 
@@ -409,11 +530,11 @@ static int xbt_dwfl_module_cb(Dwfl_Module *dwflmod,
 
 	int rc = -1;
 
-	xbt_trace("path '%s', text_offset %lx\n", arg->a_path, text_offset);
+	xbt_trace("path '%s', text_offset %lx", arg->a_path, text_offset);
 
 	dbg = dwfl_module_getdwarf(dwflmod, &dwbias);
 	if (dbg == NULL) {
-		xbt_error("cannot get DWARF context descriptor: %s\n",
+		xbt_error("cannot get DWARF context descriptor: %s",
 			  dwfl_errmsg (-1));
 		goto out;
 	}
@@ -421,7 +542,7 @@ static int xbt_dwfl_module_cb(Dwfl_Module *dwflmod,
 
 	dies = malloc(maxdies * sizeof(dies[0]));
 	if (dies == NULL) {
-		xbt_error("cannot allocate DIEs: %s\n", strerror(errno));
+		xbt_error("cannot allocate DIEs: %s", strerror(errno));
 		goto out;
 	}
 
@@ -438,7 +559,7 @@ next_cu:
 		  "version %"PRIu16", "
 		  "abbrev_offset %"PRIu64", "
 		  "address_size %"PRIu8", "
-		  "offset_size: %"PRIu8"\n",
+		  "offset_size: %"PRIu8"",
 		  (uint64_t) offset, version, abbroffset, addrsize, offsize);
 
 	offset += cuhl;
@@ -465,10 +586,11 @@ next_cu:
 			cu_base = 0;
 	}
 	dwfl_module_relocate_address(dwflmod, &cu_base);
-	xbt_trace("cu_base %lx\n", (ulong)cu_base);
+	xbt_trace("cu_base %lx", (ulong)cu_base);
 
 	do {
 		Dwarf_Die *die;
+		const char *tag_name;
 		int tag;
 		int c;
 
@@ -486,7 +608,7 @@ next_cu:
 		tag = dwarf_tag(die);
 		if (tag == DW_TAG_invalid) {
 			xbt_error("cannot get tag of DIE at offset %"PRIx64
-				  " in section '%s': %s\n",
+				  " in section '%s': %s",
 				  (uint64_t) offset, scn_name, dwarf_errmsg(-1));
 			goto out;
 		}
@@ -510,8 +632,10 @@ next_cu:
 			rel_low_pc = low_pc;
 			dwfl_module_relocate_address(dwflmod, &rel_low_pc);
 
-			/* Assume a constant relocation offset
-			 * throughout this function. */
+			/* Adjust the passed in address rather than
+			 * trying to relocate all of the DWARF
+			 * addresses. Assume a constant relocation
+			 * offset throughout this function. */
 			pc = text_offset - (rel_low_pc - low_pc);
 
 			dwarf_highpc(die, &high_pc);
@@ -522,7 +646,7 @@ next_cu:
 				goto next_die;
 
 			printf("DIE subprogam %s, offset %lx, "
-			       "pc %#lx, low_pc %#lx, high_pc %#lx\n",
+			       "pc %#lx, low_pc %#lx, high_pc %#lx",
 			       dwarf_diename(die),
 			       dwarf_dieoffset(die),
 			       (ulong) pc, (ulong)low_pc, (ulong)high_pc);
@@ -535,9 +659,13 @@ next_cu:
 			Dwarf_Op *expr[nr_exprs];
 			size_t expr_len[nr_exprs];
 
+			tag_name = (tag == DW_TAG_formal_parameter) ?
+				"parm" : "var";
+
 			loc_attr = dwarf_attr(die, DW_AT_location, &loc_attr_mem);
 			if (loc_attr == NULL) {
-				xbt_error("formal %s, offset %lx\n has no location\n",
+				xbt_error("%s %s, offset %lx\n has no location",
+					  tag_name,
 					  dwarf_diename(die),
 					  dwarf_dieoffset(die));
 				goto next_die;
@@ -547,7 +675,7 @@ next_cu:
 							 expr, expr_len, nr_exprs);
 
 			printf("DIE %s %s, offset %lx, nr_locs %d\n",
-			       tag == DW_TAG_formal_parameter ? "parm" : "var",
+			       tag_name,
 			       dwarf_diename(die),
 			       dwarf_dieoffset(die),
 			       nr_locs);
@@ -570,9 +698,8 @@ next_cu:
 			goto next_die;
 		}
 
-		printf(" [%6" PRIx64 "]  %*s%d\n",
-		       (uint64_t) offset, (int) (level * 2), "",
-		       tag);
+		printf(" [%6"PRIx64"]  %*s%d\n",
+		       (uint64_t) offset, (int) (level * 2), "", tag);
 
 #if 0
 		dwarf_getattrs(&dies[level], attr_callback, NULL, 0);
@@ -595,7 +722,7 @@ next_cu:
 				if (level-- == 0)
 					break;
 			if (c == -1) {
-				xbt_error("cannot get next DIE: %s\n", dwarf_errmsg(-1));
+				xbt_error("cannot get next DIE: %s", dwarf_errmsg(-1));
 				goto out;
 			}
 		}
@@ -624,8 +751,8 @@ int main(int argc, char *argv[])
 	mod_fd = open(mod_path, O_RDONLY);
 	if (mod_fd < 0) {
 		/* INFO? */
-		error(INFO, "cannot open '%s': %s\n",
-		      mod_path, strerror(errno));
+		xbt_error("cannot open '%s': %s",
+			  mod_path, strerror(errno));
 		goto out;
 	}
 
@@ -638,14 +765,16 @@ int main(int argc, char *argv[])
 
 	int dwfl_fd = dup(mod_fd);
 	if (dwfl_fd < 0) {
-		error(INFO, "cannot dup fd %d, '%s': %s\n",
-		      mod_fd, mod_path, strerror(errno));
+		xbt_error("cannot dup fd %d, '%s': %s",
+			  mod_fd, mod_path, strerror(errno));
 		goto out;
 	}
 
 	if (dwfl_report_offline(dwfl, mod_path, mod_path, dwfl_fd) == NULL) {
+		xbt_error("cannot load DWARF from '%s': %s",
+			  mod_path, strerror(errno)); /* XXX errno */
 		close(dwfl_fd);
-		goto out; /* ... */
+		goto out;
 	}
 
 	dwfl_report_end(dwfl, NULL, NULL);
@@ -657,6 +786,7 @@ int main(int argc, char *argv[])
 	};
 
 	dwfl_getmodules(dwfl, xbt_dwfl_module_cb, &arg, 0 /* offset*/);
+	/* ... */
 	dwfl_end(dwfl);
 out:
 	return 0;
